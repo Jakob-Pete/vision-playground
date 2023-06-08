1a6aedc03353db419f670af6b5d9d05ddb0ff8de
Jakob’s MacBook <jakob.petersen6779@stu.mtec.edu>
2023-06-07 19:04:03 -0600

    renamed contentView file

--- VisionFrameWorkPlayground/MainVisionView.swift
+++ VisionFrameWorkPlayground/MainVisionView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MainVisionView.swift
 //  VisionFrameWorkPlayground
 //
 //  Created by mac on 6/7/23.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainVisionView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -21,6 +21,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainVisionView()
     }
 }
