9c15d3d7ba192463d8f31658489a35073a7f1d71
Jakob’s MacBook <jakob.petersen6779@stu.mtec.edu>
2023-06-07 18:58:20 -0600

    initial commit

---
+++ VisionFrameWorkPlayground/VisionFrameWorkPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionFrameWorkPlaygroundApp.swift
+//  VisionFrameWorkPlayground
+//
+//  Created by mac on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct VisionFrameWorkPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
