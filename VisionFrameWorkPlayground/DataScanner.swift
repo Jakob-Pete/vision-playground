6f777830de078f7fb0746c794d61520cd3daaa49
Jakob’s MacBook <jakob.petersen6779@stu.mtec.edu>
2023-07-12 08:29:15 -0600

    Now automatically prints the highlighted code

--- VisionFrameWorkPlayground/DataScanner.swift
+++ VisionFrameWorkPlayground/DataScanner.swift
@@ -10,37 +10,44 @@ import SwiftUI
 import UIKit
 import VisionKit
 
-
 struct DataScanner: UIViewControllerRepresentable {
     
     @Binding var startScanning: Bool
-    @Binding var scanText: String
+    @Binding var scanResult: [String]
     
-    func makeUIViewController(context: Context) -> DataScannerViewController {
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> DataScannerViewController{
         let controller = DataScannerViewController(
             recognizedDataTypes: [.text()],
             qualityLevel: .balanced,
-            isHighlightingEnabled: true
-        )
+            recognizesMultipleItems: true,
+            isHighFrameRateTrackingEnabled: true,
+//            isGuidanceEnabled: true,
+            isHighlightingEnabled: true)
+        //        controller.recognizedItems
+                
+                var overlayContainerView: UIView
+        //        A view that the data scanner displays over its view without interfering with the Live Text interface.
+        
+                var regionOfInterest: CGRect?
+//                The area of the live video in view coordinates that the data scanner searches for items.
         
         controller.delegate = context.coordinator
-        
         return controller
     }
     
     func updateUIViewController(_ uiViewController: DataScannerViewController, context: Context) {
-        
         if startScanning {
             try? uiViewController.startScanning()
         } else {
             uiViewController.stopScanning()
         }
     }
-    
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-    
+   
+    @MainActor
     class Coordinator: NSObject, DataScannerViewControllerDelegate {
         var parent: DataScanner
         
@@ -48,13 +55,35 @@ struct DataScanner: UIViewControllerRepresentable {
             self.parent = parent
         }
         
-        func dataScanner(_ dataScanner: DataScannerViewController, didTapOn item: RecognizedItem) {
-            switch item {
-            case .text(let text):
-                parent.scanText = text.transcript
-            default: break
-            }
+        func dataScanner(_ dataScanner: DataScannerViewController, didAdd addedItems: [RecognizedItem], allItems: [RecognizedItem]) {
+            handleItems(allItems)
         }
         
+        func dataScanner(_ dataScanner: DataScannerViewController, didUpdate updatedItems: [RecognizedItem], allItems: [RecognizedItem]) {
+            handleItems(allItems)
+        }
+        
+        func handleItems(_ items: [RecognizedItem]) {
+            print("Items count : \(items.count)")
+            parent.scanResult = items.compactMap { $0.text }
+            items.forEach { item in
+                switch item {
+                case .text(let text):
+                    print("Scanned words: \(text.transcript)")
+                default: break
+                }
+            }
+        }
+    }
+}
+
+extension RecognizedItem {
+    var text: String? {
+        switch self {
+        case .text(let text):
+            return text.transcript
+        default:
+            return nil
+        }
     }
 }
--- VisionFrameWorkPlayground/LiveTextRecognitionView.swift
+++ VisionFrameWorkPlayground/LiveTextRecognitionView.swift
@@ -13,17 +13,16 @@ import VisionKit
 struct LiveTextRecognitionView: View {
     
     @State private var startScanning = false
-    @State private var scanText = ""
+    @State private var scanResult = [""]
     
     var body: some View {
         VStack(spacing: 0) {
-            DataScanner(startScanning: $startScanning, scanText: $scanText)
-                .frame(height: 400)
-            
-            Text(scanText)
-                .frame(minWidth: 0, maxWidth: .infinity, maxHeight: .infinity)
-                .background(in: Rectangle())
-                .backgroundStyle(Color(uiColor: .black))
+            DataScanner(startScanning: $startScanning, scanResult: $scanResult)
+            List(scanResult, id: \.self) {
+                result in
+                Text(result)
+            }
+            .frame(maxHeight: 300)
         }
         .task {
             if DataScannerViewController.isSupported && DataScannerViewController.isAvailable {
